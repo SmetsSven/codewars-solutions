d9cab072e090345f998095b5bda3241159a6a71f
SmetsSven <sven.smets001@gmail.com>
2023-01-11 23:28:03 +0100

    Added kata description

--- kata/7kyu/find_the_divisors.py
+++ kata/7kyu/find_the_divisors.py
@@ -1,3 +1,8 @@
+# https://www.codewars.com/kata/525f4206b73515bffb000b21
+# Create a function named divisors/Divisors that takes an integer n > 1
+# and returns an array with all of the integer's divisors(except for 1 and the number itself),
+# from smallest to largest. If the number is prime return the string '(integer) is prime'
+
 def divisors(integer):
     """Get array of divisors or check if prime number
     
